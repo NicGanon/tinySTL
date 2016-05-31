c2c69b20826a1bcc11588025b664785633a94b3d
NicGanon <NicGanon@gmail.com>
2016-05-31 21:24:36 +0800

    Update algorithm.h
    add a few algorithm code

--- algorithm.h
+++ algorithm.h
@@ -90,7 +90,7 @@ namespace tinySTL {
   // Non-modifying sequence operations
   template <typename InputIterator, typename Function>
   Function for_each (InputIterator first, InputIterator last, Function func) {
-    while (first < last) {
+    while (first != last) {
       func(*first);
       ++first;
     }
@@ -99,7 +99,7 @@ namespace tinySTL {
 
   template <typename InputIterator, typename T>
   InputIterator find(InputIterator first, InputIterator last, const T& val) {
-    while (first < last) {
+    while (first != last) {
       if (*first == val) {
         return first;
       }
@@ -122,10 +122,22 @@ namespace tinySTL {
 
   template <class InputIterator, class T>
   typename iterator_traits<InputIterator>::difference_type
-    count (InputIterator first, InputIterator last, const T& val) {
+  count (InputIterator first, InputIterator last, const T& val) {
     typename iterator_traits<InputIterator>::difference_type res = 0;
     while (first != last) {
       if (*first == val) {
+        ++res;
+      }
+    }
+    return res;
+  }
+
+  template <class InputIterator, class UnaryPredicate>
+  typename iterator_traits<InputIterator>::difference_type
+  count_if (InputIterator first, InputIterator last, UnaryPredicate pred) {
+    typename iterator_traits<InputIterator>::difference_type res = 0;
+    while (first != last) {
+      if (pred(*first)) {
         ++res;
       }
     }
@@ -142,6 +154,98 @@ namespace tinySTL {
       return false;
     }
   }
+
+  template <class InputIterator1, class InputIterator2>
+  pair<InputIterator1, InputIterator2>
+  mismatch (InputIterator1 first1, InputIterator1 last1,
+                InputIterator2 first2) {
+    while (first1 != last1 && *first1 == *first2) {
+      ++first1;
+      ++first2;
+    }
+    return make_pair(*first1,*first2);
+  };
+
+  template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+  pair<InputIterator1, InputIterator2>
+  mismatch (InputIterator1 first1, InputIterator1 last1,
+            InputIterator2 first2, BinaryPredicate pred) {
+    while (first1 != last1 && pred(*first1, *first2)) {
+      ++first1;
+      ++first2;
+    }
+    return make_pair(*first1,*first2);
+  };
+
+  template <class ForwardIterator>
+  ForwardIterator adjacent_find (ForwardIterator first, ForwardIterator last) {
+    ForwardIterator second = first+1;
+    while (second != last) {
+      if (*first == *second) {
+        return second;
+      }
+      first = second++;
+    }
+
+    return last;
+  }
+
+  template <class InputIterator, class OutputIterator>
+  OutputIterator copy (InputIterator first, InputIterator last,
+                       OutputIterator result) {
+    while (first != last) {
+      *result++ = *first++;
+    }
+
+    return result;
+  }
+
+  template <class T>
+  void swap (T& a, T& b) {
+    T tmp = a;
+    a = b;
+    b = tmp;
+  }
+
+  template <class ForwardIterator1, class ForwardIterator2>
+  ForwardIterator2 swap_ranges (ForwardIterator1 first1, ForwardIterator1 last1,
+                                ForwardIterator2 first2) {
+    while (first1 != last1) {
+      swap(*first1,*first2);
+      ++first1;
+      ++first2;
+    }
+
+    return first2;
+  }
+
+  template <class ForwardIterator1, class ForwardIterator2>
+  void iter_swap (ForwardIterator1 a, ForwardIterator2 b) {
+    swap(*a,*b);
+  };
+
+  template <class InputIterator, class OutputIterator, class UnaryOperation>
+  OutputIterator transform (InputIterator first1, InputIterator last1,
+                            OutputIterator result, UnaryOperation op) {
+    while (first1 != last1) {
+      *result = op(*first1);
+      ++first1;
+      ++result;
+    }
+  }
+
+  template <class InputIterator1, class InputIterator2,
+      class OutputIterator, class BinaryOperation>
+  OutputIterator transform (InputIterator1 first1, InputIterator1 last1,
+                            InputIterator2 first2, OutputIterator result,
+                            BinaryOperation binary_op) {
+    while (first1 != first2) {
+      *result = binary_op(*first1,*first2);
+      ++first1;
+      ++first2;
+      ++result;
+    }
+  };
 
 }//namespace
 
